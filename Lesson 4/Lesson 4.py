b1df2b8a19a6f7ec29bb23d9363ef30fdef8b740
movavi_school <abcsmir.ant@gmail.com>
2023-03-02 17:32:45 +0700

    02.03.2023

---
+++ Lesson 4/Lesson 4.py
@@ -0,0 +1,42 @@
+# Циклы -> while и for
+# while
+# weather = "Пасмурно"
+# day = 1
+# while weather == "Пасмурно":
+#     if day == 31:
+#         weather = "Солнечно"
+#         print("Мне не грустно")
+#         break
+#     print("Мне грустно")
+#     day = day + 1
+
+# for
+# l = [1, 2, 3]
+# s = "Это я"
+# for pelmeni in s:
+#     print(pelmeni)
+
+# for vareniki in range(5, 15 + 1):
+#     print(vareniki)
+
+# for vareniki in range(5):
+#     print(vareniki)
+
+# for vareniki in range(14, 0, -1):
+#     print(vareniki)
+
+# for vareniki in range(0, 5)
+# =
+# for vareniki in range(5)
+
+# l = [3, 7, 1, True, 4.2, "Ы"]
+# for vareniki in range(len(l)):
+#     print(l[vareniki])
+
+# for ind, arbuz in enumerate(l):
+#     print(f"Индекс: {ind}. Значение: {arbuz}")
+
+# for kartoshka in range(1, 11):
+#     if kartoshka == 4:
+#         continue # пропуск итерации
+#     print("Прыжок №", kartoshka)
